779a8613aa1c6a925e7ac5dd60a5c0966b9cce8c
ClaryGlt <kimi.goulet@gmail.com>
2023-12-22 09:23:32 +0100

    ajout move_gazebo + modif tutosim à launch (avec le add c mieux)

---
+++ tuto_kickoff/src/move_gazebo.py
@@ -0,0 +1,38 @@
+import rclpy                  # core ROS2 client python librairie
+from rclpy.node import Node   # To manipulate ROS Nodes
+# Message to publish:
+from geometry_msgs.msg import Twist
+
+
+print("move_gazebo :: START...")
+
+def main():
+    rclpy.init()     # Initialize ROS2 client
+    myNode= Node('move_gazebo_node') # Create a Node, with a name      
+
+    #Initialize a publisher:
+    velocity_publisher = myNode.create_publisher(Twist, '/cmd_vel', 10)   
+
+    # Start the ros infinit loop with myNode.
+    while True :
+        rclpy.spin_once( myNode, timeout_sec=0.1 )
+        #print("Running...")
+        # publish a msg
+        velo = Twist()
+        velo.linear.x= 0.2  # meter per second
+        velo.angular.z= 0.6 # radian per second
+        velocity_publisher.publish(velo)
+
+    # At the end, destroy the node explicitly.
+    move.destroy_node()
+
+    # and shut the light down.
+    rclpy.shutdown()
+
+    print("tuto_move :: STOP.")
+
+# activate main() function,
+# if the file is executed as a script (ie. not imported).
+if __name__ == '__main__':
+    # call main() function
+    main()
