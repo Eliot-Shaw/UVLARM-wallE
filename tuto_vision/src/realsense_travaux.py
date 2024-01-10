f94b427d40611b0172cd8cfe590ff7ee0f52a172
ClaryGlt <kimi.goulet@gmail.com>
2024-01-10 09:59:22 +0100

    test connexion node caméra

--- tuto_vision/src/realsense_travaux.py
+++ tuto_vision/src/realsense_travaux.py
@@ -35,10 +35,35 @@ class Realsense(Node):
         self.config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 60)
 
     def read_imgs(self):
-        pass
+        # Wait for a coherent tuple of frames: depth, color and accel
+        frames = self.pipeline.wait_for_frames()
+
+        color_frame = frames.first(rs.stream.color)
+        depth_frame = frames.first(rs.stream.depth)
+        
+        #################################################
+        #if not (depth_frame and color_frame):
+        #    continue
+        ################################################
+
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        depth_colormap_dim = depth_colormap.shape
+        color_colormap_dim = color_image.shape
 
     def publish_imgs(self):
-        pass
+        sys.stdout.write( f"\r- {color_colormap_dim} - {depth_colormap_dim} - ({round(freq)} fps)" )
+        # Show images
+        images = np.hstack((color_image, depth_colormap))
+        # Show images
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
+        cv2.waitKey(1)
 
     # Capture ctrl-c event
     isOk= True
