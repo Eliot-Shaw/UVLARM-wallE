af6857e8be3578d4e51c40708ccfbe2089bef79a
bot <mobisyst@imt-nord-europe.fr>
2024-01-12 15:34:18 +0100

    seuillage_node fonctionnel

--- grp_wallE_ch1/src/seuillage_node.py
+++ grp_wallE_ch1/src/seuillage_node.py
@@ -6,6 +6,10 @@ import sys, cv2, rclpy
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
+
+message = String()
+message.data = "Bouteille trouvée!"
 
 # Node Seuillage couleur verte:
 class Seuillage(Node):
@@ -13,61 +17,104 @@ class Seuillage(Node):
         super().__init__('seuillage')
 
 
-    def souris(event, x, y, flags, param):
-        global lo, hi, color, hsv_px
+    def souris(self, event, x, y, flags, param):
 
         if event == cv2.EVENT_MOUSEMOVE:
             # Conversion des trois couleurs RGB sous la souris en HSV
-            px = frame[y,x]
+            px = self.frame[y,x]
             px_array = np.uint8([[px]])
-            hsv_px = cv2.cvtColor(px_array,cv2.COLOR_BGR2HSV)
+            self.hsv_px = cv2.cvtColor(px_array,cv2.COLOR_BGR2HSV)
 
         if event==cv2.EVENT_MBUTTONDBLCLK:
-            color=image[y, x][0]
+            self.color= self.image[y, x][0]
 
         if event==cv2.EVENT_LBUTTONDOWN:
-            if color>5:
-                color-=1
+            if self.color>5:
+                self.color-=1
 
         if event==cv2.EVENT_RBUTTONDOWN:
-            if color<250:
-                color+=1
+            if self.color<250:
+                self.color+=1
 
-        lo[0]=color-10
-        hi[0]=color+10
+        self.lo[0]=self.color-15
+        self.hi[0]=self.color+15
 
+    def seuillage(self, cap):
+        self.bridge = CvBridge()
+        print("on est dans seuillage")
+        print(type(cap))
+        feur = self.bridge.imgmsg_to_cv2(img_msg=cap, desired_encoding='passthrough')
+        print(type(feur))
+        self.frame=feur
+        print("le frame ok")
+        self.image=cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+        mask=cv2.inRange(self.image, self.lo, self.hi)
+        # Flouttage de l'image
+        self.image=cv2.blur(self.image, (7, 7))
+        # Erosion d'un mask
+        mask=cv2.erode(mask, None, iterations=4)
+        # dilatation d'un mask
+        mask=cv2.dilate(mask, None, iterations=4)
+        self.image2=cv2.bitwise_and(self.frame, self.frame, mask= mask)
+        cv2.putText(self.frame, "Couleur: {:d}".format(self.color), (10, 30), cv2.FONT_HERSHEY_DUPLEX, 1, self.color_info, 1, cv2.LINE_AA)
 
-    def read_imgs(self):
-        pass
+        # Affichage des composantes HSV sous la souris sur l'image
+        pixel_hsv = " ".join(str(values) for values in self.hsv_px)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(self.frame, "px HSV: "+pixel_hsv, (10, 260),
+                font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+        elements=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        if len(elements) > 0:
+            c=max(elements, key=cv2.contourArea)
+            ((x, y), rayon)=cv2.minEnclosingCircle(c)
+            if rayon>30:
+                cv2.circle(self.image2, (int(x), int(y)), int(rayon), self.color_info, 2)
+                cv2.circle(self.frame, (int(x), int(y)), 5, self.color_info, 10)
+                cv2.line(self.frame, (int(x), int(y)), (int(x)+150, int(y)), self.color_info, 2)
+                cv2.putText(self.frame, "Bouteille !!!", (int(x)+10, int(y) -10), cv2.FONT_HERSHEY_DUPLEX, 1, self.color_info, 1, cv2.LINE_AA)
+                self.publisher_bouteille.publish(message)
 
-    def publish_img(self):
-        pass
+        cv2.imshow('Camera', self.frame)
+        cv2.imshow('image2', self.image2)
+        cv2.imshow('Mask', mask)
+
+        if cv2.waitKey(1)&0xFF==ord('q'):
+            pass
+
 
     def process_img(self):
-        self.create_subscription(Image, '/image_image', self.process_img(), 10) ###BONNNE QUESTION MON REUF###
-        self.publisher_ = self.create_publisher(Image, '/bonnequestion', 10) 
+        print("on va partir dans seuillage")
+        self.create_subscription(Image, '/image_image', self.seuillage, 10) 
+        self.publisher_bouteille = self.create_publisher(String, '/bouteille', 10)
+        print("subscribe&publish ok")
 
-        color=60 # HSV : detecter H = 60 (vert vert)
+        self.color=70 # HSV : detecter H = 60 (vert vert) pour webcam ; 80 pour realsense
 
-        lo=np.array([color-5, 100, 50])
-        hi=np.array([color+5, 255,255])
+        self.lo=np.array([self.color-5, 100, 50])
+        self.hi=np.array([self.color+5, 255,255])
 
-        color_info=(0, 0, 255)
+        self.color_info=(0, 0, 255)
 
-        cap=cv2.VideoCapture(0)
+        
         cv2.namedWindow('Camera')
-        cv2.setMouseCallback('Camera', souris)
-        hsv_px = [0,0,0]
+        cv2.setMouseCallback('Camera', self.souris)
+        self.hsv_px = [0,0,0]
 
         # Creating morphological kernel
         kernel = np.ones((3, 3), np.uint8)
 
         while True: 
             rclpy.spin_once(self, timeout_sec=0.001)
+        
+        cap.release()
+        cv2.destroyAllWindows()
+
+
 
 def main():
     rclpy.init()
     minimal_subscriber= Seuillage()
+    print("initialisation : ok")
     minimal_subscriber.process_img()
 
 if __name__ == '__main__':
