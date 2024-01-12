556c2fafb85b720b072444b0d4747321185c0bac
ClaryGlt <kimi.goulet@gmail.com>
2024-01-12 12:05:49 +0100

    avancement travail seuillage node

--- grp_wallE_ch1/src/seuillage_node.py
+++ grp_wallE_ch1/src/seuillage_node.py
@@ -13,8 +13,29 @@ class Seuillage(Node):
         super().__init__('seuillage')
 
 
-    def start_connexion(self):
-        pass
+    def souris(event, x, y, flags, param):
+        global lo, hi, color, hsv_px
+
+        if event == cv2.EVENT_MOUSEMOVE:
+            # Conversion des trois couleurs RGB sous la souris en HSV
+            px = frame[y,x]
+            px_array = np.uint8([[px]])
+            hsv_px = cv2.cvtColor(px_array,cv2.COLOR_BGR2HSV)
+
+        if event==cv2.EVENT_MBUTTONDBLCLK:
+            color=image[y, x][0]
+
+        if event==cv2.EVENT_LBUTTONDOWN:
+            if color>5:
+                color-=1
+
+        if event==cv2.EVENT_RBUTTONDOWN:
+            if color<250:
+                color+=1
+
+        lo[0]=color-10
+        hi[0]=color+10
+
 
     def read_imgs(self):
         pass
@@ -23,5 +44,32 @@ class Seuillage(Node):
         pass
 
     def process_img(self):
-        pass
+        self.create_subscription(Image, '/image_image', self.process_img(), 10) ###BONNNE QUESTION MON REUF###
+        self.publisher_ = self.create_publisher(Image, '/bonnequestion', 10) 
 
+        color=60 # HSV : detecter H = 60 (vert vert)
+
+        lo=np.array([color-5, 100, 50])
+        hi=np.array([color+5, 255,255])
+
+        color_info=(0, 0, 255)
+
+        cap=cv2.VideoCapture(0)
+        cv2.namedWindow('Camera')
+        cv2.setMouseCallback('Camera', souris)
+        hsv_px = [0,0,0]
+
+        # Creating morphological kernel
+        kernel = np.ones((3, 3), np.uint8)
+
+        while True: 
+            rclpy.spin_once(self, timeout_sec=0.001)
+
+def main():
+    rclpy.init()
+    minimal_subscriber= Seuillage()
+    minimal_subscriber.process_img()
+
+if __name__ == '__main__':
+# call main() function
+    main()
