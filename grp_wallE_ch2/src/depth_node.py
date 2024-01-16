52c87bcb7886894f65010a48db6caf58e585392d
bot <mobisyst@imt-nord-europe.fr>
2024-01-16 12:02:06 +0100

    pb trouvé

--- grp_wallE_ch2/src/depth_node.py
+++ grp_wallE_ch2/src/depth_node.py
@@ -6,6 +6,8 @@ from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float32
+from geometry_msgs.msg import Point
+
 
 message = Float32()
 
@@ -14,13 +16,15 @@ class Profondeur(Node):
     def __init__(self, fps= 60):
         super().__init__('profondeur')
 
-    def lastImg (self, image_depth_sub):
+    def lastImg (self, image_depth_sub): 
         bridge = CvBridge()
         self.cv2_image_depth = bridge.imgmsg_to_cv2(img_msg=image_depth_sub, desired_encoding='passthrough')
+#pas convertir en CV2 mais convertir en pyrelasense2
+
 
     def profondeur(self, coords_sub):
+        print(f"entrer profondeur")
         if self.cv2_image_depth is not None:
-            print(f"entrer profondeur")
             message.data = self.cv2_image_depth.get_distance(coords_sub.x, coords_sub.y)
             self.publisher_distance_bouteille.publish(message.data)
             print(f"coo ok ------------ {message}")
@@ -28,7 +32,7 @@ class Profondeur(Node):
 
     def process_img(self):
         print("on va partir dans subscribe&publish")
-        self.create_subscription(Image, '/coords_img_bouteille', self.profondeur, 10) 
+        self.create_subscription(Point, '/coords_img_bouteille', self.profondeur, 10) 
         self.create_subscription(Image, '/image_depth', self.lastImg, 10) 
         self.publisher_distance_bouteille = self.create_publisher(Float32, '/distance_bouteille', 10)
         print("subscribe&publish ok")
