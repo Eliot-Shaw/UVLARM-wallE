5c0790a780a878e8f26093e0661868e5f94cf5fc
Eliot-Shaw <eliot.shaw@proton.me>
2024-01-18 11:19:04 +0100

    transform pose a ajouter

--- Eliot-dump/transform_pose.py
+++ Eliot-dump/transform_pose.py
@@ -1,6 +1,7 @@
 import tf2_ros
 import tf2_geometry_msgs
 from geometry_msgs.msg import Point, Pose
+from visualization_msgs.msg import Marker
 
 
 def __init__(self):
@@ -29,5 +30,31 @@ def calcul_distance_bouteille(self, coords_bouteille):
     bouteille_pose.orientation.w = 0.0
 
     transform_baselink_map = tf_buffer.lookup_transform("map", "base_link", pose_stamped.header.stamp, rospy.Duration(1.0))
+                                                                            #rospy.Time.now()
 
-    bouteille_pose_transformed = tf2_geometry_msgs.do_transform_pose(bouteille_pose, transform_baselink_map)
+    self.bouteille_pose_transformed = tf2_geometry_msgs.do_transform_pose(bouteille_pose, transform_baselink_map)
+
+
+def create_bouteille_marker(self):
+    marker = Marker()
+    
+    marker.header.frame_id = "map"
+    marker.header.stamp = bouteille_pose_transformed.header.stamp
+
+    marker.type = 3 # = cylindre
+
+    # Set the scale of the marker (adjust as needed)
+    marker.scale.x = 0.1
+    marker.scale.y = 0.1
+    marker.scale.z = 0.1
+
+    # Set the color of the marker
+    marker.color.a = 1.0  # Alpha (transparency)
+    marker.color.r = 1.0  # Red
+    marker.color.g = 0.0  # Green
+    marker.color.b = 0.0  # Blue
+
+    # Set the pose of the marker based on the transformed pose
+    marker.pose = self.bouteille_pose_transformed.pose
+
+    return marker
